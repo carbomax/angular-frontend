167b0045f264d3efb068b56d36a7e6b398223a20
DESKTOP-BKAM140\Yariel Silva <yariel.usher@gmail.com>
2021-07-07 01:57:15 -0300

    some changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   URI_ROOT: 'https://dev.pepeganga-api.com/pepeganga',
-  URI_MELI: `http://auth.mercadolibre.com.uy/authorization?response_type=code&client_id=3756313271086728&state=${Math.floor(Math.random() * 100)}&redirect_uri=https://pepeganga-dropshipping.web.app/home/meli-accounts`,
+  URI_MELI: `http://auth.mercadolibre.com.uy/authorization?response_type=code&client_id=3756313271086728&state=${Math.floor(Math.random() * 100)}&redirect_uri=https://pepeganga-dropshipping-ca5f0.web.app/home/meli-accounts`,
   URI_MELI_PUBLIC: 'https://api.mercadolibre.com',
-  URI_RESET_PASS: 'https://pepeganga-dropshipping.web.app/auth/reset',
+  URI_RESET_PASS: 'https://pepeganga-dropshipping-ca5f0.web.app/auth/reset',
   URI_UPLOAD_BUCKET: 'https://dev.pepeganga-api.com/pepeganga/upload/api/bucket/download-file-from-upload-bucket?pathFile='
 };
